239e82f7ed09475e7ca76cafb6ef75e632b6db63
ones1kk <ones1k95@gmail.com>
2023-01-10 22:37:13 +0900

    Style : 포맷팅

--- moduuimarket-domain/src/test/java/com/moma/repository/MemberRepositoryTest.java
+++ moduuimarket-domain/src/test/java/com/moma/repository/MemberRepositoryTest.java
@@ -1,30 +1,30 @@
 package com.moma.repository;
 
-import static org.assertj.core.api.Assertions.*;
-
 import com.moma.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 class MemberRepositoryTest {
 
-  @Autowired
-  private MemberRepository memberRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
-  @Test
-  void add() {
-    // given
-    memberRepository.save(new Member(1L, "userA"));
+    @Test
+    void add() {
+        // given
+        memberRepository.save(new Member(1L, "userA"));
 
-    // when
-    Member saved = memberRepository.findById(1L)
-            .orElse(null);
+        // when
+        Member saved = memberRepository.findById(1L)
+                .orElse(null);
 
-    // then
-    assertThat(saved).isNotNull();
-    assertThat(saved.getId()).isEqualTo(1L);
-  }
+        // then
+        assertThat(saved).isNotNull();
+        assertThat(saved.getId()).isEqualTo(1L);
+    }
 
 }
